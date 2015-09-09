da32859351096691318177afa0e782dc93a90f5e
Bill Swartz <swartzbill2000@yahoo.com>
2015-09-09 12:47:45 -0600

    added csClient

---
+++ csClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr sp = SerialLib.SerialPortInit("COM4");
+            SerialLib.SerialPortDestroy(sp);
+            return;
+        }
+    }
+}
---
+++ csClient/SerialLib.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csClient
+{
+    public class SerialLib
+    {
+        [DllImport("SerialLib.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr SerialPortInit([MarshalAs(UnmanagedType.LPWStr)] string portName);
+
+        [DllImport("SerialLib.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern void SerialPortDestroy(IntPtr sp);
+    }
+}
