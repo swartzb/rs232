db41f9d656232639ec07c4031cac8eb0ade2bf5e
Bill Swartz <swartzbill2000@yahoo.com>
2015-09-15 15:10:53 -0600

    ready to test

--- SerialTest/MainWindow.xaml.cs
+++ SerialTest/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,33 +21,145 @@ namespace SerialTest
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
-            UInt32 err;
+            //UInt32 err;
 
-            SWL.SerialPort sp4 = new SWL.SerialPort("COM4");
-            err = sp4.Open();
-            err = sp4.Config();
-            err = sp4.Flush();
+            //SWL.SerialPort sp4 = new SWL.SerialPort("COM4");
+            //err = sp4.Open();
+            //err = sp4.Config();
+            //err = sp4.Flush();
 
-            SWL.SerialPort sp5 = new SWL.SerialPort("COM5");
-            err = sp5.Open();
-            err = sp5.Config();
-            err = sp5.Flush();
+            //SWL.SerialPort sp5 = new SWL.SerialPort("COM5");
+            //err = sp5.Open();
+            //err = sp5.Config();
+            //err = sp5.Flush();
 
-            err = sp4.Write("hello\r");
-            StringBuilder sb = new StringBuilder(1024);
-            UInt32 sbSize;
-            err = sp5.Read(sb, out sbSize);
+            //err = sp4.Write("hello\r");
+            //StringBuilder sb = new StringBuilder(1024);
+            //UInt32 sbSize;
+            //err = sp5.Read(sb, out sbSize);
 
-            err = sp4.Close();
-            err = sp5.Close();
+            //err = sp4.Close();
+            //err = sp5.Close();
 
             InitializeComponent();
 
-            this.Content = sb.ToString();
+            //this.Content = sb.ToString();
         }
+
+        public string[] PortNames
+        {
+            get
+            {
+                if (_PortNames == null)
+                {
+                    _PortNames = System.IO.Ports.SerialPort.GetPortNames();
+                }
+                return _PortNames; 
+            }
+        }
+        private string[] _PortNames = null;
+
+        public string TxMessage
+        {
+            get { return _TxMessage; }
+            set
+            {
+                if(_TxMessage != value)
+                {
+                    _TxMessage = value;
+                    RaisePropertyChanged("TxMessage");
+                }
+            }
+        }
+        private string _TxMessage = string.Empty;
+
+        public string RxMessage
+        {
+            get { return _RxMessage; }
+            set
+            {
+                if (_RxMessage != value)
+                {
+                    _RxMessage = value;
+                    RaisePropertyChanged("RxMessage");
+                }
+            }
+        }
+        private string _RxMessage = string.Empty;
+
+        AutoResetEvent RxReady;
+
+        void OnRxMessage(string msg)
+        {
+            RxMessage = msg;
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox lb = sender as ListBox;
+            btnStart.IsEnabled = (lb.SelectedItems.Count == 2);
+            return;
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            RxReady = new AutoResetEvent(false);
+
+            SWL.SerialPort spTx = new SWL.SerialPort((string)lbPorts.SelectedItems[0]);
+            uint err = spTx.Open();
+            if (err != 0)
+            {
+                return;
+            }
+
+            err = spTx.Config();
+            if (err != 0)
+            {
+                return;
+            }
+
+            err = spTx.Flush();
+            if (err != 0)
+            {
+                return;
+            }
+
+            ThreadPool.QueueUserWorkItem(
+                new WaitCallback(ThreadProc), lbPorts.SelectedItems[1]);
+            RxReady.WaitOne();
+            Thread.Sleep(TimeSpan.FromSeconds(1.0));
+
+            TxMessage = "hello";
+            err = spTx.Write(TxMessage + "\r");
+            if (err != 0)
+            {
+                return;
+            }
+
+            err = spTx.Close();
+            if (err != 0)
+            {
+                return;
+            }
+        }
+
+#region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+#endregion
+
     }
 }
---
+++ SerialTest/ThreadProc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SWL = SerialWrapperLib;
+
+namespace SerialTest
+{
+    public partial class MainWindow
+    {
+        public void ThreadProc(object state)
+        {
+            string rxPort = (string)state;
+
+            SWL.SerialPort spRx = new SWL.SerialPort(rxPort);
+            uint err = spRx.Open();
+            if (err != 0)
+            {
+                return;
+            }
+
+            err = spRx.Config();
+            if (err != 0)
+            {
+                return;
+            }
+
+            err = spRx.Flush();
+            if (err != 0)
+            {
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder(1024);
+            UInt32 sbSize;
+            RxReady.Set();
+            err = spRx.Read(sb, out sbSize);
+            if (err != 0)
+            {
+                return;
+            }
+            else
+            {
+                Action<string> onNewMessage = new Action<string>(OnRxMessage);
+                Dispatcher.Invoke(onNewMessage, sb.ToString());
+            }
+
+            err = spRx.Close();
+            if (err != 0)
+            {
+                return;
+            }
+        }
+    }
+}
